3fbb08c9ec18913835e178f0268335d01c020c1c
Lng123 <louisng1@hotmail.com>
2020-06-09 17:12:21 -0700

    basic commands

--- bot.py
+++ bot.py
@@ -4,9 +4,26 @@ import os
 
 client = commands.Bot(command_prefix= '.')
 
+#Check for events
 @client.event
 async def on_ready():
     print("Bot is ready")
+
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined the server")
+
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left the server")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"{round(client.latency * 1000)}ms")
+
+@client.command()
+async def clear(ctx, amount=2):
+    await ctx.channel.purge(limit=amount)
 # import discord
 #
 # class MyClient(discord.Client):
