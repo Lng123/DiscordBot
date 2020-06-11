99e666efb0767ed1a3de428d4e60f1f8d737b7bf
Lng123 <louisng1@hotmail.com>
2020-06-10 23:40:54 -0700

    Cogs and basic error handling

--- bot.py
+++ bot.py
@@ -18,12 +18,36 @@ async def on_member_remove(member):
     print(f"{member} has left the server")
 
 @client.command()
-async def ping(ctx):
-    await ctx.send(f"{round(client.latency * 1000)}ms")
+async def clear(ctx, amount: int):
+    await ctx.channel.purge(limit=amount)
 
 @client.command()
-async def clear(ctx, amount=2):
-    await ctx.channel.purge(limit=amount)
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Command does not exist.")
+
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please specify the amount of messages to delete.")
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
 # import discord
 #
 # class MyClient(discord.Client):
